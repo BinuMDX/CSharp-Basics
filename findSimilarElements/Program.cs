a16be07b9f0cff718e9245e57ef392f38dfe236f
Binari Dissanayake <91403558+BinuMDX@users.noreply.github.com>
2024-02-10 13:24:35 +0530

    initial commit

---
+++ findSimilarElements/Program.cs
@@ -0,0 +1,8 @@
+﻿﻿string[] orderIds = {"B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
+
+foreach(string ids in orderIds){
+   if(ids.StartsWith("B")){
+    Console.WriteLine(ids);
+    }
+}
+
